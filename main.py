983c954a93f1b40ce68c8e2c7221513647c9a67d
luster <luster@flowersky.love>
2024-07-17 22:55:05 +0800

    progress bar and url input  support

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 import urllib.request
 import threading
+from urllib.parse import urlparse
+
 
 # 下载文件的URL
-# url = 'https://example.com/largefile.zip'
-url = "https://videos.aiursoft.cn/media/original/user/anduin/5552db90ed7b494b9850f918e24ba872.mmexport1678851452849.mp4"
-# 本地保存路径
-file_name = 'largefile.zip'
+# url = 'https://videos.aiursoft.cn/media/original/user/anduin/5552db90ed7b494b9850f918e24ba872.mmexport1678851452849.mp4'
+url = input("press url to download")
+parsed_url = urlparse(url)
+file_name = parsed_url.path.split("/")[-1]
+
 
 # 获取文件大小
 def get_file_size(url):
@@ -14,24 +17,36 @@ def get_file_size(url):
     with urllib.request.urlopen(req) as response:
         return int(response.getheader('Content-Length'))
 
-# 分块下载函数
-def download_chunk(url, start, end, chunk_number):
+# 分块下载函数，显示进度
+def download_chunk(url, start, end, chunk_number, progress, lock):
     req = urllib.request.Request(url)
     req.headers['Range'] = f'bytes={start}-{end}'
     with urllib.request.urlopen(req) as response:
         with open(f'{file_name}.part{chunk_number}', 'wb') as f:
-            f.write(response.read())
+            while True:
+                data = response.read(1024)
+                if not data:
+                    break
+                f.write(data)
+                with lock:
+                    progress[chunk_number] += len(data)
+                    downloaded = sum(progress)
+                    print(f'\r下载进度: {downloaded / file_size * 100:.2f}%', end='')
 
 # 多线程下载
-def multi_thread_download(url, num_threads=8):
+def multi_thread_download(url, num_threads=4):
+    global file_size
     file_size = get_file_size(url)
     chunk_size = file_size // num_threads
+    
+    progress = [0] * num_threads
+    lock = threading.Lock()
     
     threads = []
     for i in range(num_threads):
         start = i * chunk_size
         end = start + chunk_size - 1 if i < num_threads - 1 else file_size - 1
-        thread = threading.Thread(target=download_chunk, args=(url, start, end, i))
+        thread = threading.Thread(target=download_chunk, args=(url, start, end, i, progress, lock))
         threads.append(thread)
         thread.start()
     
